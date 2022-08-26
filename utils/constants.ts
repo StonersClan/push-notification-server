202f27cc42826fb29a12a82cf6d3684df11929d2
Rushikesh Tote <rushi.tote@gmail.com>
2022-08-26 12:25:37 +0000

    fix delay

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,2 @@
 export const baseDelay = 30 * 1000; // 30 seconds
-export const maxDelay = baseDelay * Math.pow(2, 10); // 10 times
-// after 10 times, the delay will be more than 2 years
+export const maxDelay = baseDelay * Math.pow(2, 15); // 15 times
